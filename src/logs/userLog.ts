065c1675cd1795cfe6b9546c1e9445df2c136b60
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-16 18:39:12 -0400

    refactor: enhance user log functionality by including username and formatting date

--- src/logs/userLog.ts
+++ src/logs/userLog.ts
@@ -1,14 +1,17 @@
 import fs from 'fs'
 import { getCurrentTime } from '../utils/loggerUtils'
+import { formatDate } from '../utils/dateUtils'
+import { currentUser } from '../cli/cli'
 
-export const filePath: string = './data/user.log'  
+export const filePath: string = './data/user.log'
 
-export async function createUserLog(id: string, user_id: number, date: Date, action: string) {
+export async function createUserLog(id: string, date: Date, action: string) {
     try {
-        const eventString = `${getCurrentTime()} - ID: ${id}, User ID: ${user_id}, Date: ${date.toISOString()}, Action: ${action}\n`
+        const username = currentUser
+        const eventString = `${getCurrentTime()} - ID: ${id}, Usuário: ${username}, Data: ${formatDate(date)}, Ação: ${action}\n`
 
         await fs.appendFileSync(filePath, eventString, 'utf-8')
     } catch (error) {
-        console.log(`${getCurrentTime()} - Erro ao escrever no arquivo de log: ${(error as Error).message}`) 
+        console.log(`${getCurrentTime()} - Erro ao escrever no arquivo de log: ${(error as Error).message}`)
     }
 }
