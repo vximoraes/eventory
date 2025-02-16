126d35b3150512297c45113a52acff19067034b1
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-16 18:39:19 -0400

    refactor: update user seed function to improve password generation and loop condition

--- src/seeds/userSeed.ts
+++ src/seeds/userSeed.ts
@@ -1,13 +1,18 @@
-import { createUser } from "../controllers/userController";
-import { User } from "../models/userModel";
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
+import { User } from "../models/userModel"
+import { createUser } from "../controllers/userController"
 
 export async function createUserSeed() {   
-    for(let i = 0; i <= 10; i++) {
+    for(let i = 0; i < 10; i++) {
         const user: User = {
             name: faker.internet.username(),
             email: faker.internet.email(),
-            password: faker.internet.password({ length: 20, memorable: true, pattern: /[A-Za-z0-9!@#$%^&*(),.?":{}|<>]/ })
+            password: faker.internet.password({
+                length: 10, 
+                memorable: false,
+                pattern: /[A-Za-z0-9!@#$%^&*(),.?":{}|<>]/,
+                prefix: 'A1@' 
+            })
         }
 
         await createUser(user.name, user.email, user.password)
