aad448f121bd7d8affcb6c2adb45a0d17d7ecaab
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-16 18:38:35 -0400

    refactor: remove log table creation from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,15 @@
-import { createLogsTable } from "./controllers/logController"
 import { createUserTable } from "./controllers/userController"
 import { createEventTable } from "./controllers/eventController"
 import { main } from './cli/cli'
 
-// ------------- Logs ------------
-
-// Criar a tabela de logs
-createLogsTable()
-
 // ------------ Users ------------
 
-// Criar a tabela de usuários
 createUserTable()
 
-// ----------- Eventos -----------
+// ----------- Events -----------
 
-// Criar a tabela de eventos
 createEventTable()
 
 // ------------ CLI --------------
 
-// Chama a função principal da CLI  
 main()
