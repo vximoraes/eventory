e01ef819716b1b50c6e3e4f1334cec83ececc705
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-17 11:48:16 -0400

    refactor: fix formatting in userValidation by removing unnecessary whitespace

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -2,13 +2,13 @@ import { z } from "zod"
 import { User } from "../models/userModel"
 
 const userSchema = z.object({
-    name : z.string().min(1, "Nome é obrigatório"), 
-    email: z.string().email("E-mail inválido"),    
+    name: z.string().min(1, "Nome é obrigatório"),
+    email: z.string().email("E-mail inválido"),
     password: z
         .string()
-        .min(8, "A senha deve ter pelo menos 8 caracteres") 
-        .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula") 
-        .regex(/[!@#$%^&*(),.?":{}|<>]/, "A senha deve conter pelo menos um caractere especial") 
+        .min(8, "A senha deve ter pelo menos 8 caracteres")
+        .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, "A senha deve conter pelo menos um caractere especial")
 })
 
 export function validateUser(user: User) {
