8eafb26e142cbe3cee803885eccf3f169ea02d47
vinimoraes <viniciusmoraesvha@gmail.com>
2025-02-19 15:49:23 -0400

    refactor: simplify createEventLog function by removing id and using uuid for event ID

--- src/logs/eventLog.ts
+++ src/logs/eventLog.ts
@@ -1,14 +1,15 @@
 import fs from 'fs'
+import { v4 as uuid } from 'uuid'
 import { getCurrentTime } from '../utils/loggerUtils'
 import { formatDate } from '../utils/dateUtils'
 import { currentUser } from '../cli/cli'
 
 export const filePath: string = './data/evento.log'
 
-export async function createEventLog(id: string, date: Date, action: string) {
+export async function createEventLog(action: string) {
     try {
         const username = currentUser
-        const eventString = `${getCurrentTime()} - ID: ${id}, Usuário: ${username}, Data: ${formatDate(date)}, Ação: ${action}\n`
+        const eventString = `${getCurrentTime()} - ID: ${uuid()}, Usuário: ${username}, Data: ${formatDate(new Date())}, Ação: ${action}\n`
 
         await fs.appendFileSync(filePath, eventString, 'utf-8')
     } catch (error) {
