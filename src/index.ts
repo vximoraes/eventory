76398f9b25c4de86db752917435e29ecb4f1e1ef
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-02-19 20:45:38 -0400

    refactor: remove user and event table creation from index file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { createUserTable } from "./controllers/userController"
-import { createEventTable } from "./controllers/eventController"
 import { main } from './cli/cli'
-
-// ------------ Users ------------
-
-createUserTable()
-
-// ----------- Events -----------
-
-createEventTable()
 
 // ------------ CLI --------------
 
